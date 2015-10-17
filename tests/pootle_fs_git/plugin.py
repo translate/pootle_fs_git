a93323549032c23e1fc99d09771a968d84efb43f
Ryan Northey <ryan@space23.net>
2015-10-17 11:13:00 +0100

    Updating plugin tests

--- tests/pootle_fs_git/plugin.py
+++ tests/pootle_fs_git/plugin.py
@@ -12,8 +12,9 @@ import os
 import pytest
 from ConfigParser import ConfigParser
 
+from pootle_fs.models import StoreFS
 from pootle_fs_pytest.suite import (
-    run_add_test, run_fetch_test, run_rm_test)
+    run_add_test, run_fetch_test, run_rm_test, run_merge_test)
 
 from ..fixtures.plugin import tmp_git
 
@@ -34,6 +35,12 @@ def _check_git_fs(plugin, response):
                     p.fs_path.strip("/")))
             for p
             in response["pruned_from_fs"])
+
+        for response in response["pushed_to_fs"]:
+            store_fs = StoreFS.objects.get(pootle_path=response.pootle_path)
+            serialized = store_fs.store.serialize()
+            assert serialized == store_fs.file.read()
+            # todo - also check that the version in git is the same
 
 
 @pytest.mark.django
@@ -73,20 +80,44 @@ def test_plugin_read_config(git_plugin):
 # Parametrized FETCH
 @pytest.mark.django
 def test_plugin_fetch_translations(git_plugin_suite, fetch_translations):
-    fetch_translations["check_fs"] = _check_git_fs
-    run_fetch_test(git_plugin_suite, **fetch_translations)
+    run_fetch_test(
+        git_plugin_suite,
+        check_fs=_check_git_fs,
+        **fetch_translations)
 
 
 # Parametrized ADD
 @pytest.mark.django
 def test_plugin_add_translations(git_plugin_suite, add_translations):
-    add_translations["check_fs"] = _check_git_fs
-    run_add_test(git_plugin_suite, **add_translations)
+    run_add_test(
+        git_plugin_suite,
+        check_fs=_check_git_fs,
+        **add_translations)
 
 
 # Parametrized RM
 @pytest.mark.django
 def test_plugin_rm_translations(git_plugin_suite, rm_translations):
-    rm_translations["check_fs"] = _check_git_fs
-    run_rm_test(git_plugin_suite, **rm_translations)
+    run_rm_test(
+        git_plugin_suite,
+        check_fs=_check_git_fs,
+        **rm_translations)
 
+
+# Parametrized MERGE
+@pytest.mark.django
+def test_plugin_merge_fs(git_plugin_suite, merge_translations):
+    run_merge_test(
+        git_plugin_suite,
+        check_fs=_check_git_fs,
+        **merge_translations)
+
+
+# Parametrized MERGE
+@pytest.mark.django
+def test_plugin_merge_pootle(git_plugin_suite, merge_translations):
+    run_merge_test(
+        git_plugin_suite,
+        check_fs=_check_git_fs,
+        pootle_wins=True,
+        **merge_translations)
