5b55d57058a52a38ebe3b905ab4787b3752eadfa
Ryan Northey <ryan@space23.net>
2016-04-25 07:32:45 +0100

    files

--- pootle_fs_git/files.py
+++ pootle_fs_git/files.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
 import logging
 
-from pootle_fs import FSFile
+from pootle_fs.files import FSFile
 
 
 logger = logging.getLogger(__name__)
@@ -10,7 +18,7 @@ class GitFSFile(FSFile):
 
     @property
     def repo(self):
-        return self.fs.plugin.repo
+        return self.plugin.repo
 
     @property
     def latest_hash(self):
