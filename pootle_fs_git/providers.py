f436d55fdfa612d94aafd3d1a4e449141d5de89e
Ryan Northey <ryan@space23.net>
2016-07-01 08:05:33 +0100

    providers

--- pootle_fs_git/providers.py
+++ pootle_fs_git/providers.py
@@ -6,8 +6,14 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.plugin import provider
+from pootle.core.delegate import state
+from pootle.core.plugin import getter, provider
 from pootle_fs.delegate import fs_plugins
+from pootle_fs.finder import TranslationFileFinder
+from pootle_fs.matcher import FSPathMatcher
+from pootle_fs.resources import FSProjectResources
+from pootle_fs.delegate import fs_finder, fs_matcher, fs_resources
+from pootle_fs.state import ProjectFSState
 
 from .plugin import GitPlugin
 
@@ -15,3 +21,23 @@ from .plugin import GitPlugin
 @provider(fs_plugins)
 def git_plugin_provider(**kwargs):
     return dict(git=GitPlugin)
+
+
+@getter(state, sender=GitPlugin)
+def git_plugin_state_getter(**kwargs):
+    return ProjectFSState
+
+
+@getter(fs_resources, sender=GitPlugin)
+def git_resources_getter(**kwargs):
+    return FSProjectResources
+
+
+@getter(fs_finder, sender=GitPlugin)
+def git_finder_getter(**kwargs):
+    return TranslationFileFinder
+
+
+@getter(fs_matcher, sender=GitPlugin)
+def git_matcher_getter(**kwargs):
+    return FSPathMatcher
