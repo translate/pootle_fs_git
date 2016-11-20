8d0f21ed9076267105d3da1c561782def771acd0
Ryan Northey <ryan@synca.io>
2016-11-20 19:08:21 +0000

    Add plugin/repo to git fs file

--- pootle_fs_git/files.py
+++ pootle_fs_git/files.py
@@ -9,12 +9,17 @@
 import logging
 
 from pootle_fs.files import FSFile
+from pootle_fs.utils import FSPlugin
 
 
 logger = logging.getLogger(__name__)
 
 
 class GitFSFile(FSFile):
+
+    @property
+    def plugin(self):
+        return FSPlugin(self.store_fs.project)
 
     @property
     def repo(self):
