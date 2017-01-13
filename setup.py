331ea8f50c1aa40a74e1a22cf464d97e6ffecf44
Ryan Northey <ryan@synca.io>
2017-01-13 17:57:21 +0000

    Use requirements from reqs file

--- setup.py
+++ setup.py
@@ -9,8 +9,53 @@
 """Pootle filesystem plugins
 """
 
+import re
+
+from distutils import log
+
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
+
+def parse_requirements(file_name, recurse=False):
+    """Parses a pip requirements file and returns a list of packages.
+
+    Use the result of this function in the ``install_requires`` field.
+    Copied from cburgmer/pdfserver.
+    """
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        # Ignore comments, blank lines and included requirements files
+        if re.match(r'(\s*#)|(\s*$)|'
+                    '((--allow-external|--allow-unverified) .*$)', line):
+            continue
+        if re.match(r'-r .*$', line):
+            if recurse:
+                requirements.extend(parse_requirements(
+                    'requirements/' +
+                    re.sub(r'-r\s*(.*[.]txt)$', r'\1', line), recurse))
+            continue
+
+        if re.match(r'^\s*-e\s+', line):
+            requirements.append(re.sub(
+                r'''\s*-e\s+          # -e marker
+                 .*                   # URL
+                 \#egg=               # egg marker
+                 ([^\d]*)-            # \1 dep name
+                 ([\.\d]*             # \2 M.N.*
+                 ((a|b|rc|dev)+\d*)*  # (optional) devN
+                 )$''',
+                r'\1==\2', line, flags=re.VERBOSE))
+            log.warn("Pootle requires a non-PyPI dependency, when using pip "
+                     "ensure you use the --process-dependency-links option.")
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
+install_requires = parse_requirements('requirements/base.txt')
 
 setup(
     name='pootle_fs_git',
@@ -30,5 +75,5 @@ setup(
     ],
     keywords='pootle filesystem plugins',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['pootle', "gitpython==2.1.1"],
+    install_requires=install_requires,
 )
